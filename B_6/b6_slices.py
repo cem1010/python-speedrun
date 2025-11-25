35dafab4af5c50ea2b1c4006811466cafa4cf8f8
Cem Babadağlı <91885780+cem1010@users.noreply.github.com>
2025-11-25 03:25:50 +0300

    h6
    python slice

---
+++ B_6/b6_slices.py
@@ -0,0 +1,13 @@
+msg ="animal"
+print(msg[2:4])
+ssn= "#61939453900"
+print(ssn[1:12])
+
+msg2="ha!ha!ha!"
+print(msg2[2:9:3])
+print(msg2*5)
+print("hello \nworld")
+altsatır= """cem
+baba
+,"""
+print(altsatır)
